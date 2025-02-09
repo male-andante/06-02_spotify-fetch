b2b33079050c88da609770678ec3fae99a02c394
male-andante <andreadanteprivitera@gmail.com>
2025-02-09 10:22:51 +0100

    first commit

---
+++ js/spotify.js
@@ -0,0 +1,3 @@
+const eminemEndpoint  = 'https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem'
+const metallicaEndpoint = 'https://striveschool-api.herokuapp.com/api/deezer/search?q=metallica'
+const queenEndpoint = 'https://striveschool-api.herokuapp.com/api/deezer/search?q=queen'
